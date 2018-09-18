2ff65d85708f3cd42f877ea612e8671c3b5ad4d4
Mao125182510 <43335118+Mao125182510@users.noreply.github.com>
2018-09-18 16:50:27 +0800

    Add files via upload

---
+++ w02/閏年.cpp
@@ -0,0 +1,20 @@
+ #include<stdio.h>
+ 
+ int main(){
+ 	
+ 	int year ;
+ 	printf("Enter year");
+ 	scanf("%d",&year);
+ 	
+ 	if((year%4==0&&!(year%100==0))||year%400==0)
+	 {
+ 	printf("Year %d is a leap year",year);
+	 }
+ 	else {
+	 printf("Year %d is not a leap Year",year);
+	 }
+	 
+	 return 0 ;
+ 	
+ 	
+ }
